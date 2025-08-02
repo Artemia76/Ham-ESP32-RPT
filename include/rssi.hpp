0d543ad3c2fe2d417fac2ec1aa516757029b5e2b
Artemia76 <neophile76@gmail.com>
2025-08-02 19:42:39 +0200

    Test fonction transfert

---
+++ include/rssi.hpp
@@ -0,0 +1,130 @@
+/**
+ * @file rssi.hpp
+ * @author Gianni Peschiutta (F4IKZ)
+ * @brief HAM-ESP32-RPT
+ * @version 0.1
+ * @date 2025-07-20
+ * 
+ * @copyright Copyright (c) 2025
+ *
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *    This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ *    You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+#ifndef RSSI_HPP
+#define RSSI_HPP
+
+#include "log.hpp"
+
+#include <SPIFFS.h>
+#include <map>
+#include <algorithm>
+#include <SPIFFS.h>
+#include <ArduinoJson.h>
+
+struct RSSIData
+{
+    float V;
+    float dBm;
+    float S;
+};
+
+class CRSSI
+{
+public:
+
+    /**
+     * @brief Construct a new CRSSI object
+     * 
+     */
+    CRSSI();
+
+    /**
+     * @brief Destroy the CRSSI object
+     * 
+     */
+    ~CRSSI();
+
+    /**
+     * @brief Add RSSI data
+     * 
+     * @param pRSSI RSSI data to add
+     */
+    void addRSSIData(const RSSIData& pRSSI);
+
+    /**
+     * @brief Get RSSI data
+     * 
+     * @return std::map<float, RSSIData> Map of RSSI data
+     */
+    std::map<float, RSSIData> getRSSIData() const;
+    /**
+     * @brief Clear all RSSI data
+     * 
+     */
+    void clearRSSIData();  
+    /**
+     * @brief Save RSSI data to SPIFFS
+     * 
+     * @return true if save was successful, false otherwise
+     */
+    bool saveRSSIDataToSPIFFS() const;
+    /**
+     * @brief Load RSSI data from SPIFFS
+     * 
+     * @return true if load was successful, false otherwise
+     */
+    bool loadRSSIDataFromSPIFFS();
+
+
+private:
+    /**
+     * @brief Map to store RSSI data
+     * 
+     */
+    std::map<float, RSSIData> _rssiData;
+
+    /**
+     * @brief Logger instance
+     * 
+     */
+    CLog* _log = CLog::Create();
+    /**
+     * @brief Mutex to protect against concurrent access
+     * 
+     */
+    std::mutex _mutex;
+    /**
+     * @brief Prevent copy construction
+     * 
+     */
+    CRSSI(const CRSSI&) = delete;
+    /**
+     * @brief Prevent assignment
+     * 
+     */
+    CRSSI& operator=(const CRSSI&) = delete;
+    /**
+     * @brief Prevent move construction
+     * 
+     */
+    CRSSI(CRSSI&&) = delete;
+    /**
+     * @brief Prevent move assignment
+     * 
+     */
+    CRSSI& operator=(CRSSI&&) = delete;
+
+};
+
+#endif // RSSI_HPP
---
+++ src/rssi.cpp
@@ -0,0 +1,114 @@
+/**
+ * @file rssi.cpp
+ * @author Gianni Peschiutta (F4IKZ)
+ * @brief HAM-ESP32-RPT
+ * @version 0.1
+ * @date 2025-07-20
+ * 
+ * @copyright Copyright (c) 2025
+ *
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *    This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ *    You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+#include "rssi.hpp"
+
+CRSSI::CRSSI()
+{
+    _log = CLog::Create();
+    _log->Message("Starting RSSI... ");
+}
+
+CRSSI::~CRSSI()
+{
+    _log->Message("Destroying RSSI... ");
+}
+
+void CRSSI::addRSSIData(const RSSIData& pRSSI)
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    _rssiData[pRSSI.V] = pRSSI; // Use V as key
+    _log->Message("Added RSSI data: V=" + String(pRSSI.V) + ", dBm=" + String(pRSSI.dBm) + ", S=" + String(pRSSI.S), true, CLog::Level::DEBUG);
+}
+
+std::map<float, RSSIData> CRSSI::getRSSIData() const
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    return _rssiData;
+}
+
+
+void CRSSI::clearRSSIData()
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    _rssiData.clear();
+    _log->Message("Cleared all RSSI data", true, CLog::Level::DEBUG);
+}
+
+bool CRSSI::saveRSSIDataToSPIFFS() const
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    File file = SPIFFS.open("/rssi_data.json", "w");
+    if (!file) {
+        _log->Message("Failed to open file for writing", true, CLog::Level::ERROR);
+        return false;
+    }
+
+    DynamicJsonDocument doc(1024);
+    for (const auto& entry : _rssiData) {
+        JsonObject obj = doc.createNestedObject();
+        obj["dBm"] = entry.first;
+        obj["V"] = entry.second.V;
+        obj["S"] = entry.second.S;
+    }
+
+    if (serializeJson(doc, file) == 0) {
+        _log->Message("Failed to write RSSI data to file", true, CLog::Level::ERROR);
+        return false;
+    }
+
+    file.close();
+    _log->Message("RSSI data saved successfully", true, CLog::Level::DEBUG);
+    return true;
+}
+
+bool CRSSI::loadRSSIDataFromSPIFFS()
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    File file = SPIFFS.open("/rssi_data.json", "r");
+    if (!file) {
+        _log->Message("Failed to open file for reading", true, CLog::Level::ERROR);
+        return false;
+    }
+
+    DynamicJsonDocument doc(1024);
+    DeserializationError error = deserializeJson(doc, file);
+    if (error) {
+        _log->Message("Failed to parse RSSI data from file: " + String(error.c_str()), true, CLog::Level::ERROR);
+        file.close();
+        return false;
+    }
+
+    _rssiData.clear();
+    for (JsonObject obj : doc.as<JsonArray>()) {
+        RSSIData rssiData;
+        rssiData.dBm = obj["dBm"].as<float>();
+        rssiData.V = obj["V"].as<float>();
+        rssiData.S = obj["S"].as<float>();
+        addRSSIData(rssiData);
+    }
+
+    file.close();
+    _log->Message("RSSI data loaded successfully", true, CLog::Level::DEBUG);
+    return true;
+}
